9af03119116226cc759751fedc4a6385045af074
prince <princenagda35@gmail.com>
2025-09-27 17:06:17 +0530

    Goals.html completed and features of removing and adding goals is also added

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from models import db, Goal, Task
 
 app = Flask(__name__)
+# connecting database
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://prince_user:password@localhost/goals'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -11,11 +12,62 @@ db.init_app(app)
 with app.app_context():
     db.create_all()
 
+# routes
+
+# Adding goals route
+
+# display goals
+@app.route('/goals')
+def display_goals():
+    Goals = Goal.query.all()
+    return render_template('goals.html', Goals=Goals)
+
+#add goals 
+@app.route('/goals', methods=['POST', 'GET'])
+def add_goals():
+    if request.method == 'POST':
+        goal_name = request.form.get('goal_name')
+        if goal_name != '':
+            new = Goal(goals=goal_name)
+            db.session.add(new)
+            db.session.commit()
+            return redirect('/goals')
+    else:
+        return redirect('/goals')
+# delte goals
+@app.route('/delete/<int:id>', methods=['POST'])
+def delete_goals(id):
+    if request.method == 'POST':
+        goal = Goal.query.get(id)
+        if goal:
+            db.session.delete(goal)
+            db.session.commit()
+            return redirect('/goals')
+        else:
+            return redirect('/goals')
+
+# complete goals
+@app.route('/completed/<int:id>' , methods=["POST"])
+def complete_goals(id):
+    if request.method == 'POST':
+        goal = Goal.query.get(id)
+        if goal:
+            goal.completed = True
+            db.session.commit()
+            return redirect('/goals')
+        else:
+            return redirect('/goals')
+
+
+
+#Home route to display tasks
 @app.route('/')
 def home():
     Tasks = Task.query.all()
     return render_template('home.html', Tasks=Tasks)
 
+
+# Function to add and display tasks 
 @app.route('/' , methods=['POST', 'GET'])
 def add_task():
     if request.method == 'POST':
@@ -28,6 +80,9 @@ def add_task():
     else:
         return redirect('/')
     
+
+
+# Remove task function 
 @app.route('/delete/<int:id>', methods=['POST'])
 def erase(id):
     if request.method == 'POST':       
@@ -37,6 +92,8 @@ def erase(id):
             db.session.commit()
         return redirect('/')
 
+
+# Completed function 
 @app.route('/completed/<int:id>', methods=['POST'])
 def complete(id):
     if request.method == 'POST':
